b4ef473bd5af5902b274a625c8c11eadf4f9ac44
Will Howlett <wehowlett@gmail.com>
2018-05-23 19:30:28 +0100

    ugly transition

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,18 @@
-const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const common = require('./webpack.common.js');
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require("compression-webpack-plugin")
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   plugins: [
-    new UglifyJSPlugin()
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new UglifyJSPlugin(),
+    new CompressionPlugin()
   ],
   mode: 'production'
 });
+
+
