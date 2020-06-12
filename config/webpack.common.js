1728254bdd018c492535b41eb1df8098c3639a9e
Will Howlett <wehowlett@gmail.com>
2020-06-12 18:51:01 +0100

    rename mdPlugins

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -47,7 +47,7 @@ module.exports = {
           {
             loader: '@mdx-js/loader',
             options: {
-              mdPlugins: [emoji]
+              remarkPlugins: [emoji]
             }
           }
         ]
