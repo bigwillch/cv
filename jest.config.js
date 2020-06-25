b91cca2c2a877c61453cff6987df523c0dd82ac4
Will Howlett <wehowlett@gmail.com>
2020-06-25 16:00:12 +0100

    tweak

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   setupFiles: [
     './config/jest.setup.js'
-  ],
-  snapshotSerializers: [
-    'enzyme-to-json/serializer'
-  ],
-  transform: {
-    "^.+\\.js?$": "babel-jest"
-  }
+  ]
 };
